9f766c657410863016c4e1989d25689b187564b8
Siskajan <siskajan@hotmail.com>
2022-03-24 20:45:20 +0100

    fixing error PORT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/*', (req,res) => {
     res.sendFile(path.resolve(__dirname,'frontend','index.html'))
 })
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT|| 3000
 
 app.listen(PORT , () => {
     console.log(`server is running on port ${PORT} `)
